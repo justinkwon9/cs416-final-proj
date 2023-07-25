84327de8a7e3646b18ac8c61d23efb3957815b00
Justin Kwon <81657577+justinkwon9@users.noreply.github.com>
2023-07-25 00:54:59 -0700

    Update center_assists.js

--- d3/center_assists.js
+++ d3/center_assists.js
@@ -32,8 +32,8 @@ d3.csv("data/center_stats.csv").then(function(data) {
             .duration(200)
             .style("opacity", 1)
         tooltip.html(tipcontent)
-            .style("left",  (d3.pointer(event)[0] + 5) + "px") // or d3.pointer(event)[0]
-            .style("top", (d3.pointer(event)[1] - 4) + "px") // or d3.pointer(event)[1]
+            .style("left",  (d3.pointer(event)[0] + 10) + "px")
+            .style("top", (d3.pointer(event)[1] - 5) + "px")
         };
     // tooltip mouseout
     var tipMouseout = function(d) {
